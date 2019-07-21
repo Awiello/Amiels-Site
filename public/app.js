23c5ff5fc7cfbe934bec6ecdfa23dcb3570efabf
Amiel Cueto <amielcueto1997@gmail.com>
2019-07-21 20:33:18 +0800

    Done with new hamburger navigation :D

--- public/app.js
+++ public/app.js
@@ -7,5 +7,5 @@ const logo = document.querySelector("#logo");
 const tl = new TimelineMax();
 
 tl.fromTo(divisionOne,1, {height: "0%"}, {height:"80%", ease: Power2.easeInOut})
-  .fromTo(divisionOne,1.2,{width: "80%"}, {width:"80%", ease: Power2.easeInOut})
-  .fromTo(slider, 1.2,{x:"-100%"},{x:"0%", ease: Power2.easeInOut},"-=1.2");
+  .fromTo(divisionOne,1.2,{width: "100%"}, {width:"80%", ease: Power2.easeInOut})
+  .fromTo(slider, 1.2,{x:"100%"},{x:"0%", ease: Power2.easeInOut},"-=1.2");
